723cd4907088349b42add75c0004413213fff0db
José Azevedo <jos.avezaat@gmail.com>
2022-01-19 14:43:34 +0100

    Added Todo Object List Component

---
+++ src/app/todos/components/todos-body/todos-body.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodosBodyComponent } from './todos-body.component';
+
+describe('TodosBodyComponent', () => {
+  let component: TodosBodyComponent;
+  let fixture: ComponentFixture<TodosBodyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TodosBodyComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodosBodyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodosHeaderComponent } from './components/todos-header/todos-header.component';
+import { TodosBodyComponent } from './components/todos-body/todos-body.component';
 
 
 
 @NgModule({
   declarations: [
     TodosComponent,
-    TodosHeaderComponent
+    TodosHeaderComponent,
+    TodosBodyComponent
   ],
   imports: [
     CommonModule
